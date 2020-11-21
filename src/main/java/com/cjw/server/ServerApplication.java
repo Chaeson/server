0f44a289a9786f7008dce6bce43628d27740d480
Chae JongWook <only9984@gmail.com>
2020-11-21 22:32:15 +0900

    [ADD]  - MongoDB Cluster Test 및 정상 작동 체크 로직 추가.  - 확인 내역    > Connection    > Insert    > Select

--- src/main/java/com/cjw/server/ServerApplication.java
+++ src/main/java/com/cjw/server/ServerApplication.java
@@ -2,10 +2,13 @@ package com.cjw.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+// Default exclude Setting
+// If not setting this value, Spring Boot try Connect 'localhost'
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 public class ServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
---
+++ src/main/java/com/cjw/server/config/DBconfig.java
@@ -0,0 +1,83 @@
+package com.cjw.server.config;
+
+import com.mongodb.*;
+import com.mongodb.client.*;
+import com.mongodb.client.MongoClient;
+import net.minidev.json.JSONObject;
+import org.bson.Document;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DBconfig {
+
+    static Logger logger = LoggerFactory.getLogger(DBconfig.class);
+
+    public static void main(String []args){
+        // MongoDB Connection Info.
+        // MongoDB Atlas 설정한 정보로 데이터를 세팅한다.
+        ConnectionString connString = new ConnectionString(
+                "mongodb+srv://chae:4879@cluster0.aolss.mongodb.net/TestDB?retryWrites=true&w=majority"
+        );
+
+        // MongoDB API 참조값
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connString)
+                .retryWrites(true)
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+
+        // Cluster 에서 세팅한 DataBase 명
+        // 없는 경우 샘플 데이터를 만들 수 있다.
+        // RDBMS 의 스키마와 비슷한 개념으로 보임...
+        MongoDatabase database = mongoClient.getDatabase("TestDB");
+
+        // Collection 명을 설정
+        // RDBMS의 테이블? 과 비슷한 개념으로 보인다.
+        logger.info("MongoDB Name:{}",database.getCollection("TestCol"));
+
+        logger.info("MongoDB Count:{}",database.getCollection("TestCol").countDocuments());
+        logger.info("MongoDB Find:{}",database.getCollection("TestCol").find().first());
+
+        // MongoDB Insert
+        // Document 세팅
+        Map<String, Object> testInsert = new HashMap<String, Object>();
+        testInsert.put("TestInt",1);
+        testInsert.put("double", 2.0);
+        testInsert.put("testString","String");
+        testInsert.put("testDate",new Date());
+        JSONObject json = new JSONObject();
+        json.put("testKey","key");
+
+        // Document Insert
+        Document document = new Document(testInsert);
+
+        // Collection Insert
+        // database.getCollection("TestCol").insertOne(document);
+
+        logger.info("MongoDB Insert after count:{}",database.getCollection("TestCol").countDocuments());
+
+        // MongoDB Select
+        ClientSession clientSession = mongoClient.startSession();
+        MongoCollection collection =  database.getCollection("TestCol");
+
+        // Select All in a Row
+        collection.find().forEach(document1 -> {
+            logger.info("MongoDB Select Collection All:{}",document1);
+        });
+
+        // 찾을 조건을 선택한다.
+        // Document 의 TestInt 키가 1인 데이터를 찾는다.
+        FindIterable<Document> iterable = collection.find( new Document("TestInt", 1));
+
+        // TestInt가 1인 데이터의 로우
+        logger.info("MongoDB Find Result:{}",iterable.first());
+        // TestInt의 데이터가 1인 데이터의 시간 키값.
+        logger.info("MongoDB Find Result:{}",iterable.first().get("testDate"));
+
+    }
+
+}
