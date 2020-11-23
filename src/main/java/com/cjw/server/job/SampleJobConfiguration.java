d516d30e65057509fc4f9e5ec2b8b93616c59587
Chae JongWook <only9984@gmail.com>
2020-11-23 22:39:27 +0900

    [ADD]  - MongoDB Spring Batch 기본 토대 완성   > MongoItemReader / MongoIterWriter   > MongoDB Processor 구현 해볼 것!

--- src/main/java/com/cjw/server/job/SampleJobConfiguration.java
+++ src/main/java/com/cjw/server/job/SampleJobConfiguration.java
@@ -1,5 +1,6 @@
 package com.cjw.server.job;
 
+import com.cjw.server.model.SalesModel;
 import com.cjw.server.repository.SalesRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -7,11 +8,13 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.step.builder.TaskletStepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.data.MongoItemReader;
+import org.springframework.batch.item.data.MongoItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,20 +27,45 @@ public class SampleJobConfiguration {
     @Autowired
     private SalesRepository salesRepository;
 
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @Bean
-    public Job SampleJob(){
+    public Job SampleJob() throws Exception {
         return jobBuilderFactory.get("sampleJob")
                 .start(sampleStep1())
                 .build();
     }
 
     @Bean
-    public Step sampleStep1() {
+    public Step sampleStep1() throws Exception {
         return stepBuilderFactory.get("sampleStep1")
-                .tasklet((stepContribution, chunkContext) -> {
-                    log.info("Sample Step1....");
-                    return RepeatStatus.FINISHED;
-                })
+                .<SalesModel, SalesModel>chunk(1)
+                .reader(reader())
+                .writer(writer())
                 .build();
+
     }
+
+    private MongoItemWriter<SalesModel> writer() {
+        log.info("Writer!");
+        MongoItemWriter<SalesModel> mongoItemWriter = new MongoItemWriter<>();
+        mongoItemWriter.setTemplate(mongoTemplate);
+        mongoItemWriter.setCollection("sales");
+        return mongoItemWriter;
+    }
+
+    public MongoItemReader<SalesModel> reader() throws Exception {
+        log.debug("sampleStep1.... reader()");
+        MongoItemReader<SalesModel> mongoItemReader =
+                new MongoItemReader<>();
+        mongoItemReader.setTemplate(mongoTemplate);
+        mongoItemReader.setCollection("sales");
+        mongoItemReader.setTargetType(SalesModel.class);
+        mongoItemReader.setPageSize(10);
+        mongoItemReader.setQuery(new Query());
+        log.info("reader Info::{}",mongoItemReader.toString());
+        return mongoItemReader;
+    }
+
+
 }
